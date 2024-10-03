08e28b00107a318d3586c9e5804781e4ca4b98ec
codeajay <ajay01937@gmail.com>
2024-10-03 12:55:03 +0530

    access

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ app.use(cors({
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
+
+
 // const userController = require("./controllers/userController");
 
 const userRoutes = require('./routes/user');
