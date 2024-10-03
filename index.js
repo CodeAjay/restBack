abf63b56443a21a6a2635862ea5549cc6750ef34
codeajay <ajay01937@gmail.com>
2024-10-03 13:11:56 +0530

    tt aa

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ const User = require("./models/user");
 const jwt = require("jsonwebtoken");
 
 const app = express();
+
 app.use(cors({
-  origin: 'http://localhost:5173'
+  origin: 'http://localhost:5173',
+  credentials: true // Allow credentials (cookies, authorization headers)
 }));
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
