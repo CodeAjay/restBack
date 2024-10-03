f76c926223db41f23e885d77aab841e6f4ec81a9
codeajay <ajay01937@gmail.com>
2024-10-03 18:10:33 +0530

    removed firebase

--- index.js
+++ index.js
@@ -4,19 +4,7 @@ const cors = require('cors')
 require("./db");
 const User = require("./models/user");
 const jwt = require("jsonwebtoken");
-
-const admin = require('firebase-admin');
-
-// Initialize Firebase Admin SDK
-admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-  }),
-});
-
-console.log('Firebase Admin SDK initialized.');
+const config = require("../config");
 
 const app = express();
 
